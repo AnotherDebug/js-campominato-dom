1a5d20a59c5a1d4f5dd457a51f2d374116d87d69
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-19 11:48:47 +0200

    [UPDATE]: file script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,68 @@
 /* 
 **Consegna:**
 
-L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell’esercizio ma solo l’index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l’inizializzazione di git).
 
-Ogni cella ha un numero progressivo, da 1 a 100.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
 
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 
-==============================================
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba, fare visulaizzare tutte le bombe in gliglia e congelare la griglia.
 
-1. Salvo il riferimento del mio container dove inserisco dinamicamente gli elementi 'div';
+**BONUS:**
 
-2. Creo un ciclo che ad ogni iterazione genera il mio elemento 'div', che di fatto sarà un quadrato;
-   a. creo la funzione che aggiunge i quadrati, a cui vado ad applicare la classe square;
-   b. Creo una variabile che contiene la funzione per inserire dinamicamente il div;
-   c. Aggiungo square al mio container con .append();
-   
-3. Ogni quadrato avrà un evento al click, che aggiunge o toglie la classe active;
-   a. Ad ogni iterazione del ciclo c'è un evento al click;
-   b. Per richiamare il div all'evento al click, utilizzo la keyword this che è riferita a square che ha ricevuto l'evento, gli applico il metodo toggle che aggiunge o toglie la classe active dall'elemento div;
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
-4. Stampo in console il numero corrispondente della cella cliccata;
-   a. Per stampare il numero della cella corrispondente in console utilizzo l'indice, che lo passo come parametro della funzione atta a creare un nuovo elemento div;
-   b. Nel corpo della funzione creo una proprietà custom che raccoglie il valore dell'indice;
+=======================================================================================
 
-5. Creo due button, uno per generare il gioco ed uno per resettarlo;
-   a. Prendo i riferimenti dei due button;
-   b. Creo una funzione per il button reset, dove all'evento click sovrascrivo i valori contenuti nel container tramite assegnazione stringa vuota;
-   c. Tutto il ciclo lo chiudo in una funzione che verrà richiamata all'evento click del button start;
-   d. Creo una funzione per il button start, dove all'evento click faccio ripartire tutto il blocco del ciclo for contenuta in una funzione;
+1. Creo una select dove scegliere i vari livelli di difficoltà (facile, medio, difficile);
+2. Assegno alle opzioni dei valori che verranno poi letti per passare da un livello ad un altro;
 */
 
 
-//1.
+
 const containerRef = document.querySelector('.container');
 
 start();
 reset();
 
-//2.
+
 
 function init (n) {
     for (let i = 0; i < n; i++) {
-        //2. b.
+     
         const square = createSquare(i);
-        //2. c.
+      
         containerRef.append(square);
-        //3. a.
+       
         square.addEventListener ('click', function () {
-            //3. b.
+          
             this.classList.toggle('active');
             console.log(this);
-            //4.
+           
             console.log(this._index);
         });
     
     };
 };
 
-//2. a. (function) + 4. a. (index)
+
 function createSquare (index) {
     const newSquare = document.createElement('div');
     newSquare.className = 'square';
-    //4.b.
+  
     newSquare._index = index;
     return newSquare;
 };
 
-//5. b.
+
 function reset () {
-    //5. a.
+   
     const btnResetRef = document.getElementById('btnReset');
     btnResetRef.addEventListener('click', function() {
         containerRef.innerHTML = '';
@@ -79,9 +70,9 @@ function reset () {
     return btnResetRef;
 };
 
-//5. c.
+
 function start () {
-    //5. a.
+  
     const btnStartRef = document.getElementById('btnStart');
     btnStartRef.addEventListener ('click', function() {
         containerRef.innerHTML = '';
